423343cf323b71bf8d3d5e828108dc321d75e44c
GnohChengYi <chengyignoh@gmail.com>
2023-03-25 13:27:40 +0800

    feat: add scan tab to navigation bar

---
+++ frontend/m-e/m-e/m-e/ViewModels/ScanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace m_e.ViewModels
+{
+    public class ScanViewModel : BaseViewModel
+    {
+        public ScanViewModel()
+        {
+            Title = "Scan";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ frontend/m-e/m-e/m-e/Views/ScanPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace m_e.Views
+{
+    public partial class ScanPage : ContentPage
+    {
+        public ScanPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
