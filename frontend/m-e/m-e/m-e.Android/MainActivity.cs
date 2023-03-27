43c8020eb1adbb3b94570bc7e27ea492852bf005
GnohChengYi <chengyignoh@gmail.com>
2023-03-27 18:01:10 +0800

    feat: attempt to use camera, currently crash

--- frontend/m-e/m-e/m-e.Android/MainActivity.cs
+++ frontend/m-e/m-e/m-e.Android/MainActivity.cs
@@ -4,10 +4,11 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.Net.NetworkInformation;
 
 namespace m_e.Droid
 {
-    [Activity(Label = "m_e", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "m_e", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -16,11 +17,13 @@ namespace m_e.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
--- frontend/m-e/m-e/m-e/ViewModels/ScanViewModel.cs
+++ frontend/m-e/m-e/m-e/ViewModels/ScanViewModel.cs
@@ -2,6 +2,7 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using ZXing.Mobile;
 
 namespace m_e.ViewModels
 {
@@ -10,9 +11,41 @@ namespace m_e.ViewModels
         public ScanViewModel()
         {
             Title = "Scan";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            ScanResult = "nothing yet";
+            ScanCommand = new Command(() => scan());
         }
 
-        public ICommand OpenWebCommand { get; }
+        string scanResult = string.Empty;
+
+        public string ScanResult
+        {
+            get { return scanResult; }
+            set { SetProperty(ref scanResult, value); }
+        }
+
+        public ICommand ScanCommand { get; }
+
+        private async void scan()
+        {
+            // TODO flashlight permission
+            var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+            if (!status.Equals(PermissionStatus.Granted))
+            {
+                status = await Permissions.RequestAsync<Permissions.Camera>();
+                if (!status.Equals(PermissionStatus.Granted))
+                {
+                    // TODO user does not grant permission, return to home
+                    ScanResult = "no permission";
+                    return;
+                }
+            }
+            var options = new MobileBarcodeScanningOptions();   // TODO explore options
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+            ScanResult = (await scanner.Scan(options)).Text;
+        }
     }
 }
