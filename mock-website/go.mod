53663598d99fb66ca0bf576bcfbd4fe48638a21e
GnohChengYi <chengyignoh@gmail.com>
2023-03-31 16:11:42 +0800

    feat: login page display qr code

--- mock-website/go.mod
+++ mock-website/go.mod
@@ -1,3 +1,5 @@
 module v073/mock-website
 
 go 1.20
+
+require github.com/boombuler/barcode v1.0.1
--- mock-website/main.go
+++ mock-website/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"image/jpeg"
 	"net/http"
+	"os"
+
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/login", createLoginPage)
+	http.HandleFunc("/login", loginHandler)
 	http.ListenAndServe(":3000", nil)
 }
 
-func createLoginPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./login.html")
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO get token from API
+	token := "sdhahfafdadfl"
+	qrcode, _ := qr.Encode(token, qr.L, qr.Auto)
+	size := 512
+	qrcode, _ = barcode.Scale(qrcode, size, size)
+	f, _ := os.Create("qrcode.jpg")
+	defer f.Close()
+	jpeg.Encode(f, qrcode, nil)
+	http.ServeFile(w, r, "login.html")
 }
